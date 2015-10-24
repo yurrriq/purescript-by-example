a1f57ae5dec5ea941dd28206be525e9b43718687
Eric Bailey <eric@ericb.me>
2015-10-24 02:43:48 -0500

    Add diagonal

--- my-project/src/Main.purs
+++ my-project/src/Main.purs
@@ -2,5 +2,10 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff.Console
+import Math (sqrt)
+
 
 main = log "Hello, World!"
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
