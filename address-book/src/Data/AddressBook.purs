fd8ea8a8d19ac448ffb3d471961d2e15508298cc
Eric Bailey <eric@ericb.me>
2015-10-24 04:18:41 -0500

    Add findEntry

--- address-book/src/Data/AddressBook.purs
+++ address-book/src/Data/AddressBook.purs
@@ -26,6 +26,15 @@ type Address =
 emptyBook :: AddressBook
 emptyBook = empty
 
+-- | Given a first name, last name and 'AddressBook', if a matching 'Entry' is
+-- present in the given address book, return just that entry, otherwise nothing.
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry firstName lastName = head <<< filter isMatch
+  where
+    isMatch :: Entry -> Boolean
+    isMatch entry = (entry.firstName == firstName &&
+                     entry.lastName  == lastName)
+
 -- | Given an 'Entry' and an 'AddressBook', return an 'AddressBook' like the
 -- given one, with the given entry added to it.
 insertEntry :: Entry -> AddressBook -> AddressBook
