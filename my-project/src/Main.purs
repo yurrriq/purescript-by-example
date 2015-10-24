21cd6b54c1ee4b615dcc9e4855805a3df567382a
Eric Bailey <eric@ericb.me>
2015-10-24 03:07:06 -0500

    Add circleArea

--- my-project/src/Main.purs
+++ my-project/src/Main.purs
@@ -2,10 +2,13 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff.Console
-import Math (sqrt)
+import Math (pi, sqrt)
 
 
 main = print (diagonal 3.0 4.0)
+
+circleArea :: Number -> Number
+circleArea r = pi * r * r
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
