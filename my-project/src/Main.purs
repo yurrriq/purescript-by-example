9de83a62718c6ff584a5f404bf7b0b3b48593942
Eric Bailey <eric@ericb.me>
2016-04-18 02:30:53 -0500

    Make my-project pointless

--- my-project/src/Main.purs
+++ my-project/src/Main.purs
@@ -1,14 +1,26 @@
 module Main where
 
-import Prelude
-import Control.Monad.Eff.Console
+import Prelude (Unit, (*), (+), (<<<))
+import Control.Bind (join)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, print)
+import Data.Function (on)
 import Math (pi, sqrt)
 
 
+main :: forall eff. Eff (console :: CONSOLE | eff) Unit
 main = print (diagonal 3.0 4.0)
 
 circleArea :: Number -> Number
-circleArea r = pi * r * r
+circleArea = (pi * _) <<< square
 
 diagonal :: Number -> Number -> Number
-diagonal w h = sqrt (w * w + h * h)
+diagonal = sqrt <<^ (+) `on` square
+
+square :: Number -> Number
+square = join (*)
+
+binaryCompose :: forall a b c d. (c -> d) -> (a -> b -> c) -> (a -> b -> d)
+binaryCompose = (<<<) <<< (<<<)
+
+infixl 8 binaryCompose as <<^
